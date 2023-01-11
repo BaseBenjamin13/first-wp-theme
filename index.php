2764c1ccdd1f47025bc43d74ce048cbbd29e6a8e
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-11 00:02:29 -0500

    using function and wp methods

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-This is our amazing custom theme.
+<?php 
+    function intro($name, $color) {
+        echo "Hi, my name is $name, and my favorite color is $color<br></br>";
+    }
+intro("ben", "red");
+intro("jack", "blue");
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
